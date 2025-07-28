8f390aefa0cc68bbe879ea2d6a60fede869109c3
Nikhil Victus <menikhilraj05@gmail.com>
2025-07-28 06:30:58 -0500

    Completed Contact section and Email service

--- src/App.jsx
+++ src/App.jsx
@@ -8,6 +8,7 @@ import { Home } from './components/sections/Home'
 import "./index.css"
 import { About } from './components/sections/About'
 import {Projects} from './components/sections/Projects'
+import { Contact } from './components/sections/Contact'
 function App() {
   
   const [isLoaded , setIsLoaded] = useState(false);
@@ -29,6 +30,7 @@ function App() {
           <Home/>
           <About/>
           <Projects/>
+          <Contact/>
           
         </div>
     </>
